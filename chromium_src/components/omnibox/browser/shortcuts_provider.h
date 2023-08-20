eefe07aeee49cac5f7272242b2ac62e4091b2936
mkarolin <max@brave.com>
2023-08-20 13:28:07 -0400

    Fixes upstream UnusedSitePermissionsServiceTest unit tests.
    Chromium change:
    
    https://source.chromium.org/chromium/chromium/src/+/1d5717c31652a34f7f6b8aff99c6063dbfcde134
    
    commit 1d5717c31652a34f7f6b8aff99c6063dbfcde134
    Author: Tom Van Goethem <tov@chromium.org>
    Date:   Mon Jul 17 11:38:57 2023 +0000
    
        [SHv2] Move UnusedSitePermissionsService to SafetyHub
    
        This CL moves the implementation of the service and associated tests to
        the general SafetyHub folder. No functional changes.

--- chromium_src/components/omnibox/browser/shortcuts_provider.h
+++ chromium_src/components/omnibox/browser/shortcuts_provider.h
@@ -6,13 +6,12 @@
 #ifndef BRAVE_CHROMIUM_SRC_COMPONENTS_OMNIBOX_BROWSER_SHORTCUTS_PROVIDER_H_
 #define BRAVE_CHROMIUM_SRC_COMPONENTS_OMNIBOX_BROWSER_SHORTCUTS_PROVIDER_H_
 
-#define DoAutocomplete                 \
-  DoAutocompleteUnused();              \
-  friend class BraveShortcutsProvider; \
-  void DoAutocomplete
+#define kShortcutsProviderDefaultMaxRelevance \
+  kShortcutsProviderDefaultMaxRelevance;      \
+  friend class BraveShortcutsProvider
 
 #include "src/components/omnibox/browser/shortcuts_provider.h"  // IWYU pragma: export
 
-#undef DoAutocomplete
+#undef kShortcutsProviderDefaultMaxRelevance
 
 #endif  // BRAVE_CHROMIUM_SRC_COMPONENTS_OMNIBOX_BROWSER_SHORTCUTS_PROVIDER_H_
